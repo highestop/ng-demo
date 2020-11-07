b3e6e3f73eae3e0deea23c0265154007fe84a8a4
ChenYn <chenyn01@fenbi.com>
2020-11-07 21:50:12 +0800

    chore: add walkaround

--- projects/ng-demo-lib/src/util/map.ts
+++ projects/ng-demo-lib/src/util/map.ts
@@ -1,3 +1,7 @@
-export function foo(): any[] {
+// export function foo(): any[] {
+//   return [1, 2, 3].map(n => n + 1);
+// }
+
+export const foo = () => {
   return [1, 2, 3].map(n => n + 1);
-}
+};
